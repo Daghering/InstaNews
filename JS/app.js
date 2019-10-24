f08000edcf70a01103b5a7dac2dec010294625d6
Daghering <49764315+Daghering@users.noreply.github.com>
2019-10-24 12:41:19 -0400

    feat: added Javascript dropdown

--- JS/app.js
+++ JS/app.js
@@ -2,15 +2,24 @@
      $("button").on("click", function() {
         $.getJSON('https://api.nytimes.com/svc/topstories/v2/science.json?api-key=YTMJtmBtB00ippV8QvL3cjmtA9aRZvIf')
        .done(function(data) {
-        $.each(data, function(key, value) {
-              $("ul").append(`<li>${value.name}</li>`)
-              console.log(data)
+        $.each(data.results, function(key, value) {
+            $(".posts").append(`img src="....."/>`)
+            $(".posts").append(`<li>${value}</li>`)
+              console.log(data.results)
 
         })
     })
 
      })
+
+     $(function() {
+        $('#my-select-menu').on('change', function(event){
+            const $selected = $(event.target).val()
+            console.log(`You have selected ${$selected}`)
+        })
+        })
  })
+
 
         //     .fail (function() {
         //         $("ul").append(`<li>Sorry, there was a problem, please try again.</li>`)  
