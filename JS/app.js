c14bb7e2bdeeb27cd2b4f8ed9091799c6aee6040
Daghering <49764315+Daghering@users.noreply.github.com>
2019-10-24 11:13:16 -0400

    chore: added scss files

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,22 @@
-//   https://api.nytimes.com/svc/topstories/v2/science.json?api-key=YTMJtmBtB00ippV8QvL3cjmtA9aRZvIf
+ $(function() {
+     $("button").on("click", function() {
+        $.getJSON('https://api.nytimes.com/svc/topstories/v2/science.json?api-key=YTMJtmBtB00ippV8QvL3cjmtA9aRZvIf')
+       .done(function(data) {
+        $.each(data, function(key, value) {
+              $("ul").append(`<li>${value.name}</li>`)
+              console.log(data)
+
+        })
+    })
+
+     })
+ })
+
+        //     .fail (function() {
+        //         $("ul").append(`<li>Sorry, there was a problem, please try again.</li>`)  
+        
+        //      }) 
+        //      .always(function() {
+        //      $("ul").before(`<p class="loading">${"Loading..."}</p>`)  
+        //   })
+     
