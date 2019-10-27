40b7ca9bc3257f883d02036b70d59794964de3f5
Daghering <49764315+Daghering@users.noreply.github.com>
2019-10-27 15:55:19 -0400

    style: edited API images and heading for better viewing

--- JS/app.js
+++ JS/app.js
@@ -35,6 +35,21 @@
 })
 
 
+// FIXED FOOTER
+
+// $(document).ready(function () {
+//     /* vp_h will hold the height of the browser window */
+//     var vp_h = $(window).height();
+//     /* b_g will hold the height of the html body */
+//     var b_g = $('body').height();
+//     /* If the body height is lower than window */
+//     if (b_g < vp_h) {
+//         /* Set the footer css -> position: absolute; */
+//         $('.footer').css("position", "absolute");
+//     }
+// });
+
+
 
 
 
